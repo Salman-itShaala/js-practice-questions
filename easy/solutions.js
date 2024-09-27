82e6b9c32718132880855b16751df1ab000856c2
salman-itShaala <salman.shaikh@itshaala.com>
2024-09-27 11:31:43 +0530

    added solutions

---
+++ easy/solutions.js
@@ -0,0 +1,32 @@
+// question 1
+
+function add(num1, num2){
+    // console.log(num1 + num2);
+    return num1 + num2;
+}
+
+console.log(add(1,5)); // 6
+
+
+
+// question 2
+
+
+function convertToSeconds(m){
+    return m * 60;
+}
+
+
+// question 5
+
+function checkSum(num1, num2){
+    let sum = num1 + num2; //
+
+    if(sum < 100){
+        return true; 
+    }else{
+        return false;
+    }
+
+}
+
---
+++ medium/solution.js
@@ -0,0 +1,12 @@
+// question 1
+
+function findSum(input){
+    let sum = 0;
+
+    for(let i = 1; i <= input; i++){
+        sum = sum + i;
+    }
+
+    return sum;
+    
+}
